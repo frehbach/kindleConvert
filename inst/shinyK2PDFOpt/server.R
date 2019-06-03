5543e292111afb8db4aeeef01455f60970bf6d62
Frederik Rehbach <frederik.re@gmail.com>
2019-06-03 11:05:27 +0200

    Add spinners as loadingScreen

--- inst/shinyK2PDFOpt/server.R
+++ inst/shinyK2PDFOpt/server.R
@@ -1,7 +1,29 @@
 
 library(shiny)
+library(shinycssloaders)
 
 shinyServer(function(input, output) {
+    output$pdfview <- renderUI({
+        tags$iframe(style="height:600px; width:100%", src="")
+    })
+
+
+    observeEvent(input$file_input,{
+        req(input$file_input)
+
+        file.copy(input$file_input$datapath,"www", overwrite = T)
+        cmd <- paste("./k2pdfopt www/0.pdf -w 1200 -h 1584 -dpi 300 -om 0.1 -fc- -p",
+                     input$inPage,"-ui- -x",
+                     "-ml", paste0(input$mLeft,"cm"),
+                     "-mr", paste0(input$mRight,"cm"),
+                     "-mt", paste0(input$mTop,"cm"),
+                     "-mb", paste0(input$mBottom,"cm"))
+
+        output$pdfview <- renderUI({
+            system(cmd, wait = T)
+            tags$iframe(style="height:600px; width:100%", src="0_k2opt.pdf")
+        })
+    })
 
     observeEvent(input$createPreview,{
         req(input$file_input)
@@ -13,9 +35,8 @@ shinyServer(function(input, output) {
                      "-mr", paste0(input$mRight,"cm"),
                      "-mt", paste0(input$mTop,"cm"),
                      "-mb", paste0(input$mBottom,"cm"))
-        system(cmd)
-
         output$pdfview <- renderUI({
+            system(cmd, wait = T)
             tags$iframe(style="height:600px; width:100%", src="0_k2opt.pdf")
         })
     })
@@ -29,12 +50,10 @@ shinyServer(function(input, output) {
                      "-mr", paste0(input$mRight,"cm"),
                      "-mt", paste0(input$mTop,"cm"),
                      "-mb", paste0(input$mBottom,"cm"))
-        system(cmd)
-
         output$pdfview <- renderUI({
+            system(cmd, wait = T)
             tags$iframe(style="height:600px; width:100%", src="0_k2opt.pdf")
         })
-
     })
 
 })
--- inst/shinyK2PDFOpt/ui.R
+++ inst/shinyK2PDFOpt/ui.R
@@ -6,18 +6,27 @@ shinyUI(fluidPage(
 
     sidebarLayout(
         sidebarPanel(
+            h3("-- 1. --"),
             fileInput('file_input', 'upload file ( . pdf format only)', accept = c('.pdf')),
+            hr(),
+
+            h3("-- 2. --"),
+            h4("Specify file formatting"),
             numericInput("inPage","Page",value = 1, min = 1),
             numericInput("mLeft","Margin Left, cm", value = 0, min = 0, step = 0.1),
             numericInput("mRight","Margin Right, cm", value = 0, min = 0, step = 0.1),
             numericInput("mTop","Margin Top, cm", value = 0, min = 0, step = 0.1),
             numericInput("mBottom","Margin Bottom, cm", value = 0, min = 0, step = 0.1),
+
+            hr(),
+            h3("-- 3. --"),
+            h4("Create a Preview, iterate the file formatting until you are happy"),
             actionButton("createPreview", "Create Preview"),
             actionButton("createFull", "Create PDF")
         ),
 
         mainPanel(
-            uiOutput("pdfview")
+            withSpinner(uiOutput("pdfview"))
         )
     )
 ))
