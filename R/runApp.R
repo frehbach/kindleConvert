0b01abd2c4e8c6467d1b8b01903251ff5d068196
Frederik Rehbach <frederik.re@gmail.com>
2019-06-03 10:06:41 +0200

    Add Downloader

--- R/runApp.R
+++ R/runApp.R
@@ -7,3 +7,23 @@ runConverter <- function() {
 
     shiny::runApp(appDir, display.mode = "normal")
 }
+
+downloadConverter <- function(){
+    print("Please download the converter via this webpage:")
+    webPage <- "https://www.willus.com/k2pdfopt/download/"
+    browseURL(webPage)
+
+    packageDir <- paste0(system.file("",package = "shinyK2PDFOpt"), "k2pdfopt")
+
+    userReply <- readline(prompt="For this app to work, k2pdfopt will now be moved to the shinyApp folder. Please agree (y/n): ")
+
+    if(userReply == "y"){
+        if(file.exists("~/Downloads/k2pdfopt")){
+            file.copy("~/Downloads/k2pdfopt", packageDir)
+        }else{
+            error("k2pdfopt was not found in Download dir!")
+        }
+    }else{
+        error("User has to accept in order to proceed")
+    }
+}
